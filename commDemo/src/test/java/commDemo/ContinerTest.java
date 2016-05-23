37b3ece9c214f24196b1f0a347f186c26084833a
徐鸿元 <hero3752@hotmail.com>
2016-05-23 21:33:07 +0800

    continerTest

--- commDemo/src/test/java/commDemo/ContinerTest.java
+++ commDemo/src/test/java/commDemo/ContinerTest.java
@@ -1,13 +1,16 @@
 package commDemo;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,7 +30,7 @@ public class ContinerTest extends TestCase {
     public ContinerTest(String testName )
     {
 
-        super( testName );
+        super(testName);
 
         logger = LogManager.getLogger(testName);//T1.class.getName()
 
@@ -44,10 +47,87 @@ public class ContinerTest extends TestCase {
     public void test_ArrayList()
     {
 
-        List<String> list=new ArrayList<String>();
-        list.add("1");
+       // List<String> list=new ArrayList<String>();
+        //list.add("1");
 
-        assertEquals("12312121".substring(2,3),"3");
+        List<HashMap> mapList=new ArrayList<HashMap>();
+        HashMap dic1=new HashMap();
+        dic1.put("test",3);
+        mapList.add(dic1);
+        HashMap dic2=new HashMap();
+        dic2.put("test",1);
+        mapList.add(dic2);
+
+        HashMap dic3=new HashMap();
+        dic3.put("test",8);
+        mapList.add(dic3);
+        //.remove(dic3);
+
+        //List<HashMap> mapList2=new ArrayList<HashMap>();
+        for (HashMap test : mapList) {
+            Iterator iter = test.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                System.out.println(entry.getKey()+":"+entry.getValue());
+            }
+        }
+        Collections.sort(mapList,new SortByTest());
+
+        for (HashMap test : mapList) {
+            Iterator iter = test.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry entry = (Map.Entry) iter.next();
+                System.out.println(entry.getKey()+":"+entry.getValue());
+            }
+        }
+
+        List<Integer> intList=new ArrayList<Integer>();
+        intList.add(4);
+        intList.add(2);
+        intList.add(5);
+        intList.add(88);
+        intList.add(1);
+        intList.add(98);
+        intList.add(45);
+        intList.add(3);
+
+        Collections.sort(intList,new SortByTest1());
+
+        for (int temp:intList)
+        {
+            System.out.println(temp);
+
+        }
+
+
+
+        assertEquals(true,mapList.contains(dic2));
+    }
+
+    class SortByTest1 implements Comparator{
+        public int compare(Object o1, Object o2) {
+            int i1=Integer.valueOf(String.valueOf(o1));
+            int i2=Integer.valueOf(String.valueOf(o2));
+            if(i1>i2)
+                return 1;
+            return -1;
+        }
+
+    }
+
+
+
+    class SortByTest implements Comparator{
+        public int compare(Object o1, Object o2) {
+            HashMap s1 = (HashMap) o1;
+            HashMap s2 = (HashMap) o2;
+            if(s1.size()==0)return 1;
+            if(s2.size()==0)return 0;
+            if(Integer.parseInt(String.valueOf(s1.get("test")) ) >Integer.parseInt(String.valueOf(s2.get("test"))) )
+                return 1;
+            return -1;
+        }
+
     }
 
 }
